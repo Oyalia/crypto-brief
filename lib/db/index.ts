7b06d9ea99a83690e5a40cfc837fac636b2fd535
Nazarii <ipz223_sno@student.ztu.edu.ua>
2026-04-25 20:19:23 +0300

    feat: fix drizzle 1

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -2,5 +2,11 @@ import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
 
-const sql = neon(process.env.DATABASE_URL!);
+const connectionString = process.env.DATABASE_URL || process.env.POSTGRES_PRISMA_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not defined");
+}
+
+const sql = neon(connectionString);
 export const db = drizzle(sql, { schema });
