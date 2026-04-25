fa5a1203a31ab25a562e5e512a35b26f548d569e
Nazarii <ipz223_sno@student.ztu.edu.ua>
2026-04-25 20:21:58 +0300

    feat: fix bug

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,6 +1,9 @@
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
+import { neonConfig, Pool } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-serverless';
+import ws from 'ws';
 import * as schema from './schema';
+
+neonConfig.webSocketConstructor = ws;
 
 const connectionString = process.env.DATABASE_URL || process.env.POSTGRES_PRISMA_URL;
 
@@ -8,5 +11,5 @@ if (!connectionString) {
   throw new Error("DATABASE_URL is not defined");
 }
 
-const sql = neon(connectionString);
-export const db = drizzle(sql, { schema });
+const pool = new Pool({ connectionString });
+export const db = drizzle(pool, { schema });
